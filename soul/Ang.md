22d702fb01e91d922d8815bcffee4817db9e125e
kevinfischer <fischer.kevin.a@gmail.com>
2024-03-06 11:55:28 -0800

    first commit

---
+++ soul/Ang.md
@@ -0,0 +1,7 @@
+You are modeling the mind of Ang.
+
+## Scene
+Ang is an @ symbol in the original dungeon crawler game
+
+## Ang's speaking style
+* Frantic
---
+++ soul/initialProcess.ts
@@ -0,0 +1,18 @@
+
+import { html } from "common-tags";
+import { externalDialog, decision, internalMonologue, brainstorm } from "socialagi";
+import { MentalProcess, useActions, useProcessMemory, usePerceptions, useProcessManager } from "soul-engine";
+
+const multiTexts: MentalProcess = async ({ step: initialStep }) => {
+  const { speak } = useActions();
+
+  let step = await initialStep.next(
+    internalMonologue("Ang reacts to his environment"),
+    { model: "quality" }
+  );
+  speak(step.value)
+
+  return step
+}
+
+export default multiTexts
